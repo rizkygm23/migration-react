0994d29b8fa1bfe25ae598fb6acadde03870ff78
rizkygm23 <rizky230504@gmail.com>
2024-08-07 07:03:51 +0700

    add dark toggle

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,6 @@
 // src/Navbar.jsx
 import React, { useState } from 'react';
+import Toggledark from './toggledark';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,13 +12,13 @@ const Navbar = () => {
 
 
   return (
-    <header id="nav-head" className="bg-transparent max-w-screen-lg mx-auto top-0 md:rounded-full left-0 right-0 w-full lg:top-5 flex items-center z-10 justify-center fixed">
+    <header id="nav-head" className="bg-transparent max-w-screen-xl mx-auto top-0 md:rounded-full left-0 right-0 w-full lg:top-5 flex items-center z-10 justify-center fixed">
       <div className="container w-full">
-        <div className="flex items-center justify-between lg:w-full">
-          <div className="px-4">
-            <a className="font-bold font-sans text-lg text-sky-600 block py-6 tracking-wider md:opacity-0" href="#Home">Rizz</a>
+        <div className="flex dark:text-[#58A6AB] items-center justify-between lg:w-full">
+          <div className="px-4 md:before:flex-auto">
+            <a className="font-bold font-sans text-lg text-sky-600 dark:text-[#58A6AB]  block py-6 tracking-wider md:opacity-0" href="#Home">Rizz</a>
           </div>
-          <div className="flex items-center px-4 m-auto">
+          <div className="flex items-center px-4 mx-auto ">
             <button
               id="burger"
               name="burger"
@@ -29,31 +30,43 @@ const Navbar = () => {
               <span className="w-[30px] h-[2px] my-2 block bg-sky-950 transition duration-300 ease-in-out"></span>
               <span className="origin-bottom-left w-[30px] h-[2px] my-2 block bg-sky-950 transition duration-300 ease-in-out"></span>
             </button>
-            <nav className={`${isOpen ? 'block' : 'hidden'} absolute p-4 right-4 top-20 py-5 bg-slate-100 shadow-lg rounded-lg max-w-[250px] w-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none`} id="nav-menu">
+            <nav className={`${isOpen ? 'block' : 'hidden'}  absolute p-4 right-4 top-20 py-5 bg-slate-100 dark:bg-slate-800 dark:lg:bg-transparent shadow-lg rounded-lg max-w-[250px] w-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none md:mx-auto`} id="nav-menu">
               <ul className="block lg:flex ">
-                <li className="py-3 px-3 rounded-lg hover:bg-slate-200 group lg:hover:bg-none">
-                  <a className="mt-4 font-medium font-sans text-black group-hover:text-sky-600" href="#aboutme">About Me 🔍
-                    <div className="rounded-lg w-full h-0.5 bg-sky-600/80 scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out group-hover:h-1"></div>
+                <li className="py-3 px-3 rounded-lg hover:bg-slate-200 group md:hover:bg-transparent">
+                  <a className="mt-4 font-medium font-sans text-black dark:text-[#58A6AB] group-hover:text-sky-500 dark:group-hover:text-[#58A6AB]" href="#aboutme">About Me 🔍
+                    <div className="rounded-lg w-full h-0.5 bg-sky-500 dark:bg-[#58A6AB] scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out group-hover:h-1"></div>
                   </a>
                 </li>
-                <li className="py-3 px-3 rounded-lg hover:bg-slate-200 group">
-                  <a className="mt-4 font-medium font-sans group-hover:text-sky-600" href="#uiux">UI/UX Designer 🎨
-                    <div className="rounded-lg w-full h-0.5 bg-sky-600/80 scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out group-hover:h-1"></div>
+                <li className="py-3 px-3 rounded-lg hover:bg-slate-200 group md:hover:bg-transparent">
+                  <a className="mt-4 font-medium font-sans group-hover:text-sky-500 dark:group-hover:text-[#58A6AB]" href="#uiux">UI/UX Designer 🎨
+                    <div className="rounded-lg w-full h-0.5 bg-sky-500 dark:bg-[#58A6AB] scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out group-hover:h-1"></div>
                   </a>
                 </li>
-                <li className="py-3 px-3 rounded-lg hover:bg-slate-200 group">
-                  <a className="mt-4 font-medium font-sans group-hover:text-sky-600" href="#nodes">Blockchain Validator 🔐
-                    <div className="rounded-lg w-full h-0.5 bg-sky-600/80 scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out group-hover:h-1"></div>
+                <li className="py-3 px-3 rounded-lg hover:bg-slate-200 group md:hover:bg-transparent">
+                  <a className="mt-4 font-medium font-sans group-hover:text-sky-500 dark:group-hover:text-[#58A6AB]" href="#nodes">Blockchain Validator 🔐
+                    <div className="rounded-lg w-full h-0.5 bg-sky-500 dark:bg-[#58A6AB] scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out group-hover:h-1"></div>
                   </a>
                 </li>
+                <div className="py-3 px-3 rounded-lg lg:hidden hover:bg-slate-200 group md:hover:bg-transparent right-0">
+                  <Toggledark />
+                </div>
+
+        
+
               </ul>
+
+              
             </nav>
           </div>
-          <div id='new element' className="px-4 hidden lg:block">
+
+          <div id='new element' className="px-4 hidden  lg:flex ">
+            <div className="py-3 px-3 rounded-lg hover:bg-slate-200 group md:hover:bg-transparent right-0">
+                  <Toggledark />
+                </div>
             <a className="" href="#Home">
               <button onClick={()=>{
                 window.location.href = 'mailto:rizky230504@gmail.com';
-              }} className="overflow-hidden flex animate-float text-sky-900 buttons bottom-40 w-fullitems-center border border-sky-500 px-4 py-4 rounded-full items-center  shadow-sky-400 font-semibold shadow-sm hover:bg-slate-100 backdrop-blur-md hover:shadow-sky-500 hover:shadow-sm hover:text-sky-400">
+              }} className="overflow-hidden flex dark:bg-[#58A6AB] animate-float text-sky-900 buttons bottom-40 w-fullitems-center border border-sky-500 px-4 py-4 rounded-full items-center  shadow-sky-400 font-semibold shadow-sm hover:bg-slate-100 backdrop-blur-md hover:shadow-sky-500 hover:shadow-sm  dark:text-slate-200 dark:hover:bg-[#77b2b7]">
                 
                 <h1>
                   Get in Touch
@@ -62,6 +75,7 @@ const Navbar = () => {
               </button>
             </a>
           </div>
+
         </div>
       </div>
     </header>
--- src/mainSection.jsx
+++ src/mainSection.jsx
@@ -22,9 +22,9 @@ const MainSection = () => {
         viewport={{once:false, amount:0.7}}
        className="container w-full ">
         <div className="max-w-screen-sm mx-auto grid grid-cols-1 py-11">
-          <h3 className="font-sans block font-semibold text-sky-500 text-xl sm:text-2xl md:text-3xl">Hi, my name is</h3>
-          <h1 className="font-sans font-bold text-gray-900 text-2xl sm:text-3xl mt-1 md:text-4xl">MUHAMMAD RIZKY.</h1>
-          <h2 className="font-sans font-medium text-gray-500 text-xl sm:text-2xl mt-4 md:text-3xl lg:mt-10 h-14">
+          <h3 className="font-sans block font-semibold text-sky-500 dark:text-[#58A6AB] text-xl sm:text-2xl md:text-3xl">Hi, my name is</h3>
+          <h1 className="font-sans font-bold text-gray-900 text-2xl dark:text-[#C6C6C6] sm:text-3xl mt-1 md:text-4xl">MUHAMMAD RIZKY.</h1>
+          <h2 className="font-sans font-medium text-[#6C6C6C] dark:text-[#6C6C6C] text-xl sm:text-2xl mt-4 md:text-3xl lg:mt-10 h-14">
             {textss}
             <span Cursor='>' > <Cursor cursorStyle='/>'/>  </span>
           </h2>
@@ -39,17 +39,17 @@ const MainSection = () => {
               whileInView={"show"}
               viewport={{once:false, amount:0.1}}
               whileHover="hover"
-       className="container card-wrapped w-96 h-96 max-w-xs overflow-hidden mx-auto bg-transparent mt-10 grid grid-cols-1 rounded-lg shadow-lg shadow-sky-200 hover:shadow-md hover:shadow-sky-300">
+       className="container card-wrapped w-96 h-96 max-w-xs overflow-hidden mx-auto bg-transparent mt-10 grid grid-cols-1 rounded-lg shadow-md shadow-sky-400 dark:shadow-[#58A6AB] hover:shadow-lg hover:shadow-sky-500">
         <div className="card-content p-10 rounded-lg mx-auto ">
-          <h2 className="text-center text-lg font-semibold mt-10 font-sans text-sky-500">MY WORKS</h2>
+          <h2 className="text-center text-lg font-semibold mt-10 font-sans text-sky-500 dark:text-[#58A6AB]">MY WORKS</h2>
           <a href="#uiux" className="">
-            <button className="border border-sky-500 p-2 rounded-3xl mt-10 hover:bg-slate-700 w-full my-auto ">
-              <h3 className="text-lg font-sans font-normal text-sky-500">UI/UX Designer</h3>
+            <button className="border border-sky-500 dark:border-[#58A6AB] p-2 rounded-3xl mt-10 hover:bg-slate-700 w-full my-auto ">
+              <h3 className="text-lg font-sans font-normal text-sky-500 dark:text-[#58A6AB]">UI/UX Designer</h3>
             </button>
           </a>
           <a href="#uiux" className="">
-            <button className="border border-sky-500 p-2 rounded-3xl mt-5 hover:bg-slate-700 w-full my-auto">
-              <h3 className="text-lg font-sans font-normal text-sky-500">Blockchain Validator</h3>
+            <button className="border border-sky-500 dark:border-[#58A6AB] p-2 rounded-3xl mt-5 hover:bg-slate-700 w-full my-auto">
+              <h3 className="text-lg font-sans font-normal text-sky-500 dark:text-[#58A6AB]">Blockchain Validator</h3>
             </button>
           </a>
         </div>
